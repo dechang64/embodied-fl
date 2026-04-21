6c4146e5e19974ead06c0cefbbc239aeb69a2322
Dechang Xu <dechang64@users.noreply.github.com>
2026-04-21 15:24:59 +0800

    Delete build.rs

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    tonic_build::configure()
-        .compile(&["proto/embodied_fl.proto"], &["proto"])
-        .unwrap_or_else(|e| {
-            eprintln!("Failed to compile proto: {}", e);
-            std::process::exit(1);
-        });
-}
